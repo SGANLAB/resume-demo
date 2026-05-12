0b0d708a02dc49e06793187a4baefae830e212d7
sg <1324794116@qq.com>
2026-05-12 11:25:20 +0800

    Add resume website demo

---
+++ script.js
@@ -0,0 +1,8 @@
+const backToTopButton = document.querySelector(".back-to-top");
+
+backToTopButton?.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
